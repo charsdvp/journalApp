df2318ccea3d9ad89cf6eef726875bda68f41c47
charsdvp <carlossanchezdvp@gmail.com>
2023-11-15 19:57:22 -0600

    NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate omnis eligendi quas necessitatibus ipsam similique molestias ex temporibus dignissimos dolores? Accusantium voluptatibus ea quae aspernatur est odio reprehenderit quaerat vero?</Typography> */}
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
