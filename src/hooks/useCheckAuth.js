8f5fe2271796405c903d8e6baa2a468a39b17467
charsdvp <carlossanchezdvp@gmail.com>
2023-11-23 14:05:35 -0600

    add hook useCheckAuth

---
+++ src/hooks/useCheckAuth.js
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { FirebaseAuth } from '../firebase/config'
+import { login, logout } from '../store/auth/authSlice'
+
+export const useCheckAuth = () => {
+  const { status } = useSelector(state => state.auth)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    // cuando el estado de la aplicacion cambia
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      // si no tengo un usuario activo se dispara logout
+      if (!user) return dispatch(logout())
+      // si tengo un usuario activo
+      const { uid, displayName, name, photoURL } = user
+      dispatch(login({ uid, displayName, name, photoURL }))
+    })
+  }, [])
+  return { status }
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,26 +1,11 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
-import { useDispatch, useSelector } from 'react-redux'
-import { CheckingAuth } from '../ui/'
-import { useEffect } from 'react'
-import { onAuthStateChanged } from 'firebase/auth'
-import { FirebaseAuth } from '../firebase/config'
-import { login, logout } from '../store/auth/authSlice'
+import { useCheckAuth } from '../hooks'
+import { CheckingAuth } from '../ui'
 
 export const AppRouter = () => {
-  const { status } = useSelector(state => state.auth)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    // cuando el estado de la aplicacion cambia
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      // si no tengo un usuario activo se dispara logout
-      if (!user) return dispatch(logout())
-      // si tengo un usuario activo
-      const { uid, displayName, name, photoURL } = user
-      dispatch(login({ uid, displayName, name, photoURL }))
-    })
-  }, [])
+  const { status } = useCheckAuth()
   if (status === 'checking') {
     return <CheckingAuth />
   }
