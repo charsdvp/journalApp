cdfcb283b1e089561594afa349aa3921b28cb7ef
charsdvp <carlossanchezdvp@gmail.com>
2023-11-23 13:57:35 -0600

    Manteniendo el estado de autenticacion de la aplicacion

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,42 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { CheckingAuth } from '../ui/'
+import { useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { FirebaseAuth } from '../firebase/config'
+import { login, logout } from '../store/auth/authSlice'
 
 export const AppRouter = () => {
   const { status } = useSelector(state => state.auth)
-
+  const dispatch = useDispatch()
+  useEffect(() => {
+    // cuando el estado de la aplicacion cambia
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      // si no tengo un usuario activo se dispara logout
+      if (!user) return dispatch(logout())
+      // si tengo un usuario activo
+      const { uid, displayName, name, photoURL } = user
+      dispatch(login({ uid, displayName, name, photoURL }))
+    })
+  }, [])
   if (status === 'checking') {
     return <CheckingAuth />
   }
   return (
     <>
       <Routes>
+        {
+          status === 'authenticated'
+            ? <Route path='/*' element={<JournalRoutes />} />
+            : <Route path='/auth/*' element={<AuthRoutes />} />
+        }
+        <Route path='/*' element={<Navigate to='/auth/login' />} />
         {/* Login y Registro */}
-        <Route path='/auth/*' element={<AuthRoutes />} />
+        {/* <Route path='/auth/*' element={<AuthRoutes />} /> */}
         {/* Journal App */}
-        <Route path='/*' element={<JournalRoutes />} />
+        {/* <Route path='/*' element={<JournalRoutes />} /> */}
       </Routes>
     </>
   )
--- src/ui/components/CheckingAuth.jsx
+++ src/ui/components/CheckingAuth.jsx
@@ -1,6 +1,7 @@
 import { CircularProgress, Grid } from '@mui/material'
 
 export const CheckingAuth = () => {
+  // loader
   return (
     <Grid
     container
