bb52c38ff500a67e1facb05214b474d6c257ee5d
charsdvp <carlossanchezdvp@gmail.com>
2023-11-24 10:49:22 -0600

    Crear nueva nota

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,14 +2,20 @@ import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 import { AddOutlined } from '@mui/icons-material'
+import { startNewNote } from '../../store/journal/thunks'
+import { useDispatch } from 'react-redux'
 
 export const JournalPage = () => {
+  const dispatch = useDispatch()
+  const onClickNewNote = () => {
+    dispatch(startNewNote())
+  }
   return (
     <JournalLayout>
       {/* <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate omnis eligendi quas necessitatibus ipsam similique molestias ex temporibus dignissimos dolores? Accusantium voluptatibus ea quae aspernatur est odio reprehenderit quaerat vero?</Typography> */}
       <NothingSelectedView />
       {/* <NoteView /> */}
-      <IconButton size='large' sx={{ color: 'white', backgroundColor: 'error.main', ':hover': { backgroundColor: 'error.main' }, opacity: 0.9, position: 'fixed', right: 50, bottom: 50 }}>
+      <IconButton onClick={onClickNewNote} size='large' sx={{ color: 'white', backgroundColor: 'error.main', ':hover': { backgroundColor: 'error.main' }, opacity: 0.9, position: 'fixed', right: 50, bottom: 50 }}>
         <AddOutlined sx={{ fontSize: 30 }}/>
       </IconButton>
     </JournalLayout>
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,12 +1,23 @@
+import { collection, doc, setDoc } from 'firebase/firestore/lite'
+import { FirebaseDB } from '../../firebase/config'
+
 // inicia el proceso de crear una nueva nota
 export const startNewNote = () => {
-  return async (dispatch) => {
-    // uid
+  return async (dispatch, getState) => {
+    // console.log(getState()) // trae de vuelta todo lo que tengamos en nuestra store
+    // queremos el uid de authentication
+    const { uid } = getState().auth
+
     const newNote = {
       title: '',
       body: '',
       date: new Date().getTime()
     }
+    // creamos la referencia al documento de firebase
+    const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`))
+    // agrega la nota a firebase
+    const setDocResp = await setDoc(newDoc, newNote)
+    console.log({ newDoc, setDocResp })
     // dispatch
     // dispatch(newNote)
     // dispatch(activarNote)
